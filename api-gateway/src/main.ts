fb0bf70d93fb19eeb8a139d4e8a7d6cf8f77d99a
Roman Stelchenko <R.Stelchenko@gpsolutions.com>
2025-12-29 13:20:12 +0100

    [00.Final.Project] add branch with metrics

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -3,6 +3,7 @@ import { json, Request, Response, NextFunction } from 'express';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Counter, Histogram, Registry, collectDefaultMetrics } from 'prom-client';
 import { AppModule } from './app.module';
 import { RequestIdMiddleware } from './common/request-id.middleware';
 import { LoggerService } from './common/logger.service';
@@ -11,9 +12,45 @@ import { HttpExceptionFilter } from './common/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const logger = app.get(LoggerService);
+  const metricsRegistry = new Registry();
+  collectDefaultMetrics({ register: metricsRegistry });
+  const httpRequestsTotal = new Counter({
+    name: 'http_server_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'route', 'status_code'],
+    registers: [metricsRegistry],
+  });
+  const httpRequestDuration = new Histogram({
+    name: 'http_server_request_duration_seconds',
+    help: 'HTTP request duration in seconds',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5],
+    registers: [metricsRegistry],
+  });
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.get('/metrics', async (_req: Request, res: Response) => {
+    res.set('Content-Type', metricsRegistry.contentType);
+    res.send(await metricsRegistry.metrics());
+  });
 
   app.use(json({ limit: process.env.BODY_LIMIT || '1mb' }));
   app.use(RequestIdMiddleware);
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const start = process.hrtime();
+    res.on('finish', () => {
+      const [seconds, nanos] = process.hrtime(start);
+      const duration = seconds + nanos / 1e9;
+      const route = (req.route && req.route.path) || req.path;
+      const labels = {
+        method: req.method,
+        route,
+        status_code: res.statusCode.toString(),
+      };
+      httpRequestsTotal.inc(labels);
+      httpRequestDuration.observe(labels, duration);
+    });
+    next();
+  });
   app.use((req: Request & { requestId?: string }, res: Response, next: NextFunction) => {
     const start = Date.now();
     res.on('finish', () => {
--- player-service/src/main.ts
+++ player-service/src/main.ts
@@ -1,11 +1,49 @@
 import 'reflect-metadata';
+import { Request, Response, NextFunction } from 'express';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Counter, Histogram, Registry, collectDefaultMetrics } from 'prom-client';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const metricsRegistry = new Registry();
+  collectDefaultMetrics({ register: metricsRegistry });
+  const httpRequestsTotal = new Counter({
+    name: 'http_server_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'route', 'status_code'],
+    registers: [metricsRegistry],
+  });
+  const httpRequestDuration = new Histogram({
+    name: 'http_server_request_duration_seconds',
+    help: 'HTTP request duration in seconds',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5],
+    registers: [metricsRegistry],
+  });
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.get('/metrics', async (_req: Request, res: Response) => {
+    res.set('Content-Type', metricsRegistry.contentType);
+    res.send(await metricsRegistry.metrics());
+  });
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const start = process.hrtime();
+    res.on('finish', () => {
+      const [seconds, nanos] = process.hrtime(start);
+      const duration = seconds + nanos / 1e9;
+      const route = (req.route && req.route.path) || req.path;
+      const labels = {
+        method: req.method,
+        route,
+        status_code: res.statusCode.toString(),
+      };
+      httpRequestsTotal.inc(labels);
+      httpRequestDuration.observe(labels, duration);
+    });
+    next();
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
